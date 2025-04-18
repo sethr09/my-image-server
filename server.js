286c601789f506411812ec4835c2c1879749e3c6
sethr09 <rajahbala05@gmail.com>
2025-04-18 10:49:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidv4 } = require('uuid'); // For generating unique filenames (inst
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
